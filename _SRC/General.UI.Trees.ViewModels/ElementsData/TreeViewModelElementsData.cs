634b675a04c2959d67be325d0eaae231d8f802d6
Richard PAYET <rick.info.rsoc@gmail.com>
2024-02-18 21:38:03 +0100

    V1.1.0 : ajout GetNumericInfo et GetStringInfo à TreeViewModelElementsData

--- _SRC/General.UI.Trees.ViewModels/ElementsData/TreeViewModelElementsData.cs
+++ _SRC/General.UI.Trees.ViewModels/ElementsData/TreeViewModelElementsData.cs
@@ -2,8 +2,20 @@
 
 public record TreeViewModelElementsData
 {
-    public string Text { get; set; } = null!;
-    public string? Hint { get; set; }
+    public string Id { get; init; }
+    public int Depth { get; init; }
 
-    public int Depth { get; set; }
+    public Dictionary<string, string> StringInfos { get; init; } = null!;
+    public Dictionary<string, int> NumericInfos { get; init; } = null!;
+
+    public string? GetStringInfo(string infoKey)
+    {
+        StringInfos.TryGetValue(infoKey, out string? info);
+        return info;
+    }
+    public int? GetNumericInfo(string infoKey)
+    {
+        var found = NumericInfos.TryGetValue(infoKey, out int info);
+        return (found)? info : null;
+    }
 }
