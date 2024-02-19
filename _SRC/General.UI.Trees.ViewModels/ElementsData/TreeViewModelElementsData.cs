3281b27cf5b7730dd6465aa9e813bc08c83c45f3
Richard PAYET <rick.info.rsoc@gmail.com>
2024-02-19 01:33:33 +0100

    General.UI.Trees.ViewModels v2.0.0 : Nettoyage de TreeViewModelElementsData, conservation uniqt des property : Label et Depth.

--- _SRC/General.UI.Trees.ViewModels/ElementsData/TreeViewModelElementsData.cs
+++ _SRC/General.UI.Trees.ViewModels/ElementsData/TreeViewModelElementsData.cs
@@ -2,23 +2,7 @@
 
 public record TreeViewModelElementsData
 {
-    public string Id { get; init; } = null!;
-
     public string Label { get; init; } = null!;
 
     public int Depth { get; init; }
-
-    public Dictionary<string, string> StringInfos { get; init; } = null!;
-    public Dictionary<string, int> NumericInfos { get; init; } = null!;
-
-    public string? GetStringInfo(string infoKey)
-    {
-        StringInfos.TryGetValue(infoKey, out string? info);
-        return info;
-    }
-    public int? GetNumericInfo(string infoKey)
-    {
-        var found = NumericInfos.TryGetValue(infoKey, out int info);
-        return (found)? info : null;
-    }
 }
