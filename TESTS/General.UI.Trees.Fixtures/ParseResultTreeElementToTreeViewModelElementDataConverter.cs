e191f311509219b3c5b55dd8d945f90d0860db38
Richard PAYET <rick.info.rsoc@gmail.com>
2024-02-19 14:57:15 +0100

    TESTS: General.UI.Trees.Fixtures

---
+++ TESTS/General.UI.Trees.Fixtures/ParseResultTreeElementToTreeViewModelElementDataConverter.cs
@@ -0,0 +1,41 @@
+﻿using General.UI.Trees.Presenters.SequenceParsing.Interfaces;
+using General.UI.Trees.ViewModels.ElementsData;
+
+
+namespace General.UI.Trees.Fixtures;
+
+public class ParseResultTreeElementToTreeViewModelElementDataConverter : IParseResultTreeElementToTreeViewModelElementDataConverter<TreeViewModelElementsData>
+{
+    public TreeViewModelElementsData Convert(General.SequenceParsing.Generic.RootNode<char> parseResultRootNode, string treeTitle)
+    {
+        var label = treeTitle;
+        var result = new TreeViewModelElementsData
+        {
+            Depth = parseResultRootNode.Depth,
+            Label = $"{label}"
+        };
+        return result;
+    }
+
+    public TreeViewModelElementsData Convert(General.SequenceParsing.Generic.Node<char> parseResultNode, int indexInParent)
+    {
+        var label = $"{parseResultNode.GetSimplifiedStateAsString()}";
+        var result = new TreeViewModelElementsData
+        {
+            Depth = parseResultNode.Depth,
+            Label = $"{label}"
+        };
+        return result;
+    }
+
+    public TreeViewModelElementsData Convert(General.SequenceParsing.Generic.Leaf<char> parseResultLeaf, int indexInParent)
+    {
+        var label = $"Data{parseResultLeaf.GetDataAsString()}";
+        var result = new TreeViewModelElementsData
+        {
+            Depth = parseResultLeaf.Depth,
+            Label = $"{label}"
+        };
+        return result;
+    }
+}
---
+++ TESTS/General.UI.Trees.Fixtures/Parsing.cs
@@ -0,0 +1,45 @@
+﻿using General.SequenceParsing.Generic;
+using General.SequenceParsing.Char;
+
+
+namespace General.UI.Trees.Fixtures;
+
+
+public static class Parsing
+{
+    public static ParseResult<char> GetParseResult()
+    {
+        string stringToParse = "xyzAAwktAAFbzBBejAA123AAF567BBFrien";
+        CharSequenceParser parser = GetParser(stringToParse);
+
+        StringMatchingEvaluator stringMatchingEvaluator = new StringMatchingEvaluatorByEquality();
+
+        CharBlockDelimiter blockAStartDelimiter = CharBlockDelimiter.Create("AA", stringMatchingEvaluator);
+        CharBlockDelimiter blockAEndDelimiter = CharBlockDelimiter.Create("AAF", stringMatchingEvaluator);
+        CharBlock blockA = CharBlock.Create(blockAStartDelimiter, blockAEndDelimiter);
+
+        CharBlockDelimiter blockBStartDelimiter = CharBlockDelimiter.Create("BB", stringMatchingEvaluator);
+        CharBlockDelimiter blockBEndDelimiter = CharBlockDelimiter.Create("BBF", stringMatchingEvaluator);
+        CharBlock blockB = CharBlock.Create(blockBStartDelimiter, blockBEndDelimiter, expectedInnerBlocks: CharBlocks.Create(new List<CharBlock> { blockA }));
+
+        CharBlocks expectedInnerBlocks = CharBlocks.Create(new List<CharBlock> { blockA, blockB });
+
+
+        ParseResult<char> parseResult = parser.Parse(expectedInnerBlocks);
+
+        return parseResult;
+    }
+
+    private static CharSequenceParser GetParser(string stringToParse)
+    {
+        CharSequence charSequenceToParse = CharSequence.Create(stringToParse);
+        CharSequenceParser parser = GetParser(charSequenceToParse);
+        return parser;
+    }
+
+    private static CharSequenceParser GetParser(CharSequence charSequenceToParse)
+    {
+        CharSequenceParser parser = CharSequenceParser.Create(charSequenceToParse);
+        return parser;
+    }
+}
